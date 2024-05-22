72615c6c1e7c64ea852079c72f2b8efb27486a60
HATIM ILYAS <125820069+HatimIlyas04@users.noreply.github.com>
2024-05-22 17:00:39 +0200

    Update Home.jsx

--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -5,7 +5,7 @@ import useMedia from "../../Hooks/useMedia";
 import CreateInvoice from "../CreateInvoice/CreateInvoice";
 import HeaderMenu from "./HeaderMenu";
 import ListOfInvoices from "./ListOfInvoices"; 
-
+ 
 const Home = () => {   
   const { modal } = useSelector((state) => state.modal);
   const mobile = useMedia("(max-width: 700px)");
